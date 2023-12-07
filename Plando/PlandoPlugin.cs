3e0f71c12370b3930cb00aabd79e2092b9510ce9
Diogo Pinela <diogoid7400@gmail.com>
2023-12-07 02:23:46 +0000

    Initial version

---
+++ Plando/PlandoPlugin.cs
@@ -0,0 +1,19 @@
+using Bep = BepInEx;
+using HL = HarmonyLib;
+using IC = DeathsDoor.ItemChanger;
+using AGM = DeathsDoor.AlternativeGameModes;
+
+namespace DeathsDoor.Plando;
+
+[Bep.BepInPlugin("deathsdoor.plando", "Plando", "1.0.0.0")]
+internal class CheckLoggerPlugin : Bep.BaseUnityPlugin
+{
+    public void Start()
+    {
+        AGM.AlternativeGameModes.Add("START PLANDO", () =>
+        {
+            var data = IC.SaveData.Open();
+            data.Placements.Add(new() { ItemName = "Hookshot", LocationName = "Grove of Spirits Door" });
+        });
+    }
+}
